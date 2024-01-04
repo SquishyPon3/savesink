62a316c417b30f7ef1ffd2c587f6e90badce66d5
Kitt3 <vinylshock@mail.com>
2024-01-03 23:28:52 -0800

    Moved file / directory setup code into funcs outside of main

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 use std::{path::{Path, PathBuf}, process::exit, io::Write};
-use pancurses::initscr;
+use cursive::{reexports::time::OffsetDateTime, logger::init};
+use pancurses::{initscr, Window};
 use dirs_next::document_dir;
 use std::fs::File;
 
 // struct User {
 //     name: String
-// }
-
-// struct Game {
-//     name: String,
-//     save_path: Path,
 // }
 
 struct SaveDir {
@@ -35,10 +31,35 @@ impl SaveDir {
     }
 }
 
-fn main() {
-    // Creates a kind of gross console window, might not be ideal.
-    let window = initscr();
-    let saves;
+struct Save {
+    name: String,
+    save_data: Vec<SaveData>
+}
+
+impl Save {
+    fn new (name: &str, path: &Path) -> Self {
+        return Self { 
+            name: name.to_string(), 
+            // TODO: Create based on the save folders found in  
+            // $home/documents/savesink/saves/{Self.name}
+            save_data: Vec::new()
+        }
+    }
+}
+
+enum Source {
+    Local,
+    Remote
+}
+
+struct SaveData {
+    id: u32,
+    source: Source,
+    creation_date: OffsetDateTime
+}
+
+fn init_dir(window: &Window) -> Option<SaveDir> {
+    let saves: SaveDir;
 
     match SaveDir::new() {
         None => {
@@ -74,20 +95,35 @@ fn main() {
         println!("Savesink folder successfully found.");
     }
     println!("{}\n", saves.path.display());
-
-    // Find file structure ini / create if it does not exist.
-    // save_map is used to define the specific file path for each game's saves
-    // Will need commands for listing all contents of the save_map.toml,
-    // creating a new save / path, editing an existing save / path, deleting a save / path, 
-    // renaming save, etc
-    let save_map;
     
+    return Some(saves);
+}
+
+fn init_save_map(saves: &SaveDir) -> Option<File>{
+    let save_map;
+
     if Path::new(".\\save_map.toml").exists() {
         save_map = File::open(saves.path.join("save_map.toml")).unwrap();
     }
     else {
         save_map = File::create(saves.path.join("save_map.toml")).unwrap();
     }
+
+    return Some(save_map);
+}
+
+fn main() {
+    // Creates a kind of gross console window, might not be ideal.
+    let window = initscr();
+    // Should handle cases where the save directory could not be found / made
+    let saves = init_dir(&window).unwrap();
+
+    // Find file structure ini / create if it does not exist.
+    // save_map is used to define the specific file path for each game's saves
+    // Will need commands for listing all contents of the save_map.toml,
+    // creating a new save / path, editing an existing save / path, deleting a save / path, 
+    // renaming save, etc
+    let save_map = init_save_map(&saves).unwrap();
 
     println!("Press any key to exit...");
     window.getch();
